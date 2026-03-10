c90a34d10c8b687318e157490a082126fd04ebac
yaiza <100496015@alumnos.uc3m.es>
2026-03-10 09:13:46 +0100

    muchas fotos y buscador

--- app/src/main/java/com/uc3m/it/babyfood/FoodActivity.kt
+++ app/src/main/java/com/uc3m/it/babyfood/FoodActivity.kt
@@ -2,20 +2,23 @@ package com.uc3m.it.babyfood
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 
 
 class FoodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_food)
+        enableEdgeToEdge() //, para que el contenido pueda ir hasta los bordes de la pantalla.
+        setContentView(R.layout.activity_food) //este será el layout que se sará como interfaz
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -36,90 +39,105 @@ class FoodActivity : AppCompatActivity() {
             "undécimo mes",
             "duodécimo mes"
         )
-
+//adapter que mostrará las opciones en el spinner
         val adapter = ArrayAdapter(
             this,
             android.R.layout.simple_spinner_item,
             optionsMonths
         )
         val selectOpts: Spinner = findViewById<Spinner>(R.id.spinnerMonths)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        selectOpts.adapter = adapter
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)//es un layout XML que Google incluye dentro del propio framework de Android.
+        selectOpts.adapter = adapter//Conectas el Spinner con el adapter para que muestre las opciones.
 
 
         val recyclerView: RecyclerView = findViewById<RecyclerView>(R.id.recyclerFoods)
+        val searchEditText: EditText = findViewById(R.id.editSearch)//el buscador
 
-        // Lista de alimentos (texto en español)
+
+        // Lista de alimentos
         val foodList = listOf(
             // Verduras y Hortalizas
-            Food("Zanahoria"),
-            Food("Brócoli"),
-            Food("Patata"),
-            Food("Tomate"),
-            Food("Calabaza"),
-            Food("Calabacín"),
-            Food("Berenjena"),
-            Food("Guisantes"),
-            Food("Judías verdes"),
-            Food("Puerro"),
-            Food("Batata"),
-            Food("Coliflor"),
-            Food("Pepino"),
+            Food("Zanahoria", R.drawable.z_zanahoria),
+            Food("Brócoli",R.drawable.z_brocoli),
+            Food("Patata", R.drawable.z_patata),
+            Food("Tomate",R.drawable.z_tomate),
+            Food("Calabaza",R.drawable.z_calabaza),
+            Food("Calabacín",R.drawable.z_calabacin),
+            Food("Berenjena",R.drawable.z_berenjena),
+            Food("Guisantes",R.drawable.z_guisantes),
+            Food("Judías verdes",R.drawable.z_judiasverdes),
+            Food("Puerro",R.drawable.z_puerro),
+            Food("Batata",R.drawable.z_batata),
+            Food("Coliflor",R.drawable.z_coliflor),
+            Food("Pepino",R.drawable.z_pepino),
 
             // Frutas
-            Food("Plátano"),
-            Food("Manzana"),
-            Food("Pera"),
-            Food("Naranja"),
-            Food("Mandarina"),
-            Food("Uva"),
-            Food("Aguacate"),
-            Food("Mango"),
-            Food("Papaya"),
-            Food("Fresa"),
-            Food("Arándanos"),
-            Food("Ciruela"),
-            Food("Melocotón"),
-            Food("Sandía"),
-            Food("Melón"),
+            Food("Plátano",R.drawable.z_platano),
+            Food("Manzana", R.drawable.z_manzana),
+            Food("Pera",R.drawable.z_pera),
+            Food("Naranja",R.drawable.z_naranja),
+            Food("Uva",R.drawable.z_uva),
+            Food("Aguacate",R.drawable.z_aguacate),
+            Food("Mango",R.drawable.z_mango),
+            Food("Papaya",R.drawable.z_papaya),
+            Food("Fresa",R.drawable.z_fresa),
+            Food("Arándanos",R.drawable.z_arandano),
+            Food("Ciruela",R.drawable.z_ciruela),
+            Food("Melocotón",R.drawable.z_melocoton),
+            Food("Sandía",R.drawable.z_sandia),
+            Food("Melón",R.drawable.z_melon),
+            Food("Limon",R.drawable.z_limon),
+            Food("Kiwi",R.drawable.z_kiwi),
 
             // Proteínas (Carnes, Pescados y Legumbres)
-            Food("Pollo"),
-            Food("Vacuno"),
-            Food("Cerdo"),
-            Food("Pavo"),
-            Food("Conejo"),
-            Food("Huevo"),
-            Food("Merluza"),
-            Food("Salmón"),
-            Food("Lentejas"),
-            Food("Garbanzos"),
-            Food("Alubias"),
-            Food("Tofu"),
+            Food("Pollo", R.drawable.z_pollo),
+            Food("Vacuno", R.drawable.z_vacuno),
+            Food("Cerdo",R.drawable.z_cerdo),
+            Food("Pavo",R.drawable.z_pavo),
+            Food("Conejo",R.drawable.z_conejo),
+            Food("Huevo",R.drawable.z_huevo),
+            Food("Merluza",R.drawable.z_merluza),
+            Food("Salmón",R.drawable.z_salmon),
+            Food("Lentejas",R.drawable.z_lentejas),
+            Food("Garbanzos",R.drawable.z_garbanzos),
+            Food("Alubias",R.drawable.z_alubias),
+            Food("Tofu",R.drawable.z_tofu),
 
             // Cereales y Lácteos
-            Food("Maíz"),
-            Food("Arroz"),
-            Food("Avena"),
-            Food("Pasta"),
-            Food("Quinoa"),
-            Food("Cuscús"),
-            Food("Pan"),
-            Food("Leche"),
-            Food("Yogur natural"),
-            Food("Queso fresco"),
+            Food("Maíz",R.drawable.z_maiz),
+            Food("Arroz",R.drawable.z_arroz),
+            Food("Avena",R.drawable.z_avena),
+            Food("Pasta",R.drawable.z_pasta),
+            Food("Quinoa",R.drawable.z_quinoa),
+            Food("Cuscús",R.drawable.z_cuscus),
+            Food("Pan",R.drawable.z_pan),
+            Food("Leche",R.drawable.z_leche),
+            Food("Yogur natural",R.drawable.z_yougurt),
+            Food("Queso fresco",R.drawable.z_queso),
 
             // Otros
-            Food("Aceite de oliva")
+            Food("Aceite de oliva",R.drawable.z_aceite)
         )
 
-// Grid de 2 columnas
-        recyclerView.layoutManager = GridLayoutManager(this, 2)
-
 // Adapter
-        recyclerView.adapter = FoodAdapter(foodList)
+        val foodAdapter = FoodAdapter(foodList) //el adapter le meto la lista
+        // Grid de 2 columnas
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.adapter = foodAdapter
+
+        //listener para la búsqueda
+        searchEditText.addTextChangedListener { text ->
+
+            val filteredList = foodList.filter { food ->
+                food.name.contains(
+                    text.toString(),
+                    ignoreCase = true //hace que no importe mayúsculas/minúsculas
+                )
+            }
+            foodAdapter.updateList(filteredList)
+
     }
 
-
+    }
 
 }
--- app/src/main/java/com/uc3m/it/babyfood/FoodAdapter.kt
+++ app/src/main/java/com/uc3m/it/babyfood/FoodAdapter.kt
@@ -5,13 +5,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import android.widget.ImageView
 
 class FoodAdapter(
-    val items: List<Food>
+    private var items: List<Food>//por que con val no pero con private var si
 ) : RecyclerView.Adapter<FoodAdapter.FoodViewHolder>() {
 
     class FoodViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textName: TextView = itemView.findViewById<TextView>(R.id.textFoodName)
+        val textName: TextView = itemView.findViewById(R.id.textFoodName)
+        val image: ImageView = itemView.findViewById(R.id.imageFood)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
@@ -22,9 +25,28 @@ class FoodAdapter(
 
     override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
         val food = items[position]
+
         holder.textName.text = food.name
+        holder.image.setImageResource(food.imageRes)
+
+        // Cambiar fondo según selección
+        if (food.isSelected) {
+            holder.itemView.setBackgroundResource(R.drawable.item_food_selected)
+        } else {
+            holder.itemView.setBackgroundResource(R.drawable.item_food_normal)
+        }
+
+        holder.itemView.setOnClickListener {
+            food.isSelected = !food.isSelected
+            notifyItemChanged(position)
+        }
     }
 
     override fun getItemCount(): Int = items.size
+
+    fun updateList(newList: List<Food>) {
+        items = newList
+        notifyDataSetChanged()
+    }
 }
 
